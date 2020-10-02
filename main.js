fb6c388af3df12b2c184cb996d674180c87b87ee
pinargultekin <pinargultekin@yahoo.com>
2020-10-01 22:52:56 -0400

    error box added

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 
     $("#input-text").click(function(){
         $("#email").attr("placeholder", "Invalid email input");
-    })   
+    })   ;
+
+    $("#append-icon").click(function(){
+        $("#warning").css("display", "flex");
+        $("#input-text").css("border", "1px solid #4c2982");
+        $("#email").attr("placeholder", "Invalid email input");
+    });
 
 })
